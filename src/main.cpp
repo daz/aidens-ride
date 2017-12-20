2187958dededdc83e227af424ba0b2073675f70f
Darren Jeacocke <dazonic@me.com>
2017-12-20 19:45:30 +1000

    :lipstick:

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,19 @@
 // Delay between adjusting speed in milliseconds.
 #define RAMP_INTERVAL   5
 
+// Declare possible directions
 enum Direction {
   Forwards,
   Reverse
 };
 
+// Declare possible ramp states
 enum RampState {
   Accelerating,
   Decelerating
 };
 
+// Declare function that gets current selected button
 Direction getSelectedDirection();
 
 int currentSpeed = 0;
@@ -36,7 +39,9 @@ void setup() {
 
   pinMode(ACCELERATOR_PIN, INPUT);
   pinMode(DIR_SELECT_PIN, INPUT);
+  pinMode(MAX_SPEED_PIN, INPUT);
   pinMode(MOTOR_DIR_PIN, OUTPUT);
+  pinMode(MOTOR_SPEED_PIN, OUTPUT);
 
   prevDirection = getSelectedDirection();
 }
@@ -90,8 +95,6 @@ void loop() {
 
     // Cap speed to what's selected on the potentiometer
     currentSpeed = constrain(currentSpeed, 0, maxSpeed);
-
-    Serial.println(currentSpeed);
   }
 
   // Send speed to the motor
